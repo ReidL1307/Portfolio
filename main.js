ea7fb5968011316ada47f0a2829512d5c7f3d4d8
ReidL1307 <79946812+ReidL1307@users.noreply.github.com>
2022-08-20 23:34:18 -0600

    Changes made by Reid Leahy

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+//boilerplate
+
 import './style.css'
 
 import * as three from "three";
+import gsap from "gsap";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
@@ -23,6 +26,8 @@ const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath('/examples/js/libs/draco/');
 loader.setDRACOLoader(dracoLoader);
 
+//load models/background
+
 loader.load("./models/office.glb", function (gltf) {
     gltf.scene.castShadow = true;
     scene.add(gltf.scene);
@@ -39,14 +44,33 @@ light.position.set(5, 3, -3);
 light.castShadow = true;
 scene.add(light);
 
-const controls = new OrbitControls(camera, renderer.domElement);
+//const controls = new OrbitControls(camera, renderer.domElement);
 
 scene.background = new three.Color(0x73d966);
 
+//event listener
+
+camera.position.set(2, 2, -2);
+camera.rotation.set(-2.356194490192345, 0.6154797086703874, 2.6179938779914944);
+
+
+gsap.to(camera.position, {
+    x: -0.4,
+    y: 0.7,
+    z: 0,
+    duration: 1.5,
+    onUpdate: function () {
+        camera.lookAt(0, 0.5, 0);
+    }
+})
+
+//animate stuff
+
 function animate() {
     requestAnimationFrame(animate);
-    controls.update();
+    //controls.update();
     renderer.render(scene, camera);
+    console.log(camera.rotation);
 };
 
 animate();
